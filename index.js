2bbedd3b75ea1c923f700fe1422ec9f904b5459f
ShawnTownsend <stitans28@gmail.com>
2021-10-12 14:21:09 +0000

    Completed questions 1-3

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 //1
 function sumOfThreeAndFive(){
-  
+    let sum = 0
+    for (let i = 1, i < 1000, i++){
+        if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+    }
+}  
+    return sum
 }
 
-function greaterNum(){
-  
+function greaterNum(num1, num2){
+    if (isNaN(num1, num2)) {
+    }if (num1 < num2) {
+    return num2
+    } else if (num1 > num2) {
+    return num1
+    } else {
+    return("both integers are equal");
+}
+} else
+    return 'null'
 }
 
 function oddAndEvenInFifteen(){
-  
+  for (var x = 0; x <= 15; x++) {
+        if (x === 0) {
+                console.log(x +  " is even");
+        }
+        else if (x % 2 === 0) {
+                console.log(x + " is even");   
+        }
+        else {
+                console.log(x + " is odd");
+        }
 }
+}
+console.log()
 
 function sortThreeNums(){
   
